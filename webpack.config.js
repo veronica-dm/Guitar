8f626a6a566bc93b9cac5a25f95f971edd9ae4a6
Veronica <vero.dm@yandex.ru>
2024-05-28 23:20:41 +0300

    banner-right ver1

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = ({develop}) => ({
         {
             test: /\.(woff|woff2|eot|ttf|otf)$/i,
             type: 'asset/resource',
-          },
+        },
     ]
   },
   ...devServer(develop),
