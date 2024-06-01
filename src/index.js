fe4da68e804b1c5f5f8ba0fcfa8e628e09fdb0c1
Veronica <vero.dm@yandex.ru>
2024-06-01 11:16:57 +0300

    slides 1 ver

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
+// import 'normalize.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss';
+
+import Swiper from 'swiper';
+import { Navigation, Pagination } from 'swiper/modules';
+
+// init Swiper:
+const swiper = new Swiper('.slider-teacher', {
+    // configure Swiper to use modules
+    slidesPerView: 3,
+    spaceBetween: 30,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+  });
