7b84896560966c269aa682f8cd8a71b6f30eb93a
Veronica <vero.dm@yandex.ru>
2024-06-03 00:11:42 +0300

    shedule ver1

--- src/index.js
+++ src/index.js
@@ -18,3 +18,21 @@ const swiper = new Swiper('.slider-teacher', {
     },
     modules: [Navigation, Pagination],
   });
+
+  // Accordion
+
+var acc = document.getElementsByClassName("accordion__btn");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+ acc[i].addEventListener("click", function() {
+   this.classList.toggle("active");
+   var panel = this.nextElementSibling;
+   if (panel.style.maxHeight) {
+     panel.style.maxHeight = null;
+   } else {
+     panel.style.maxHeight = panel.scrollHeight + "px";
+   }
+ });
+}
+
